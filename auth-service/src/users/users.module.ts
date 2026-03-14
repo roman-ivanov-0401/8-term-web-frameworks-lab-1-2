f080668609176c111c80c3d8f3df7c280d0030ce
PavelTkachenk0 <128927786+PavelTkachenk0@users.noreply.github.com>
2026-03-14 20:26:49 +0300

    доработал содание пользователя

--- auth-service/src/users/users.module.ts
+++ auth-service/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { UserProfile } from './user-profile.entity';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserProfile])],
   providers: [UsersService],
   exports: [UsersService],
 })
